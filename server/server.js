3402cf168000cef8ff34bc5b43e632a396705f7b
smog29 <kuba175174@gmail.com>
2024-12-05 15:02:20 +0100

    Add local dev db setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const { Pool } = require('pg');  // Import the PostgreSQL client
+
+const app = express();
 const corsOptions = {
-  origin: ['http://localhost:5173']
+  origin: ['http://localhost:5173'],
 };
 
 app.use(cors(corsOptions));
 
-app.get('/api', (req, res) => {
-  res.json({ 'fruits': ['orange', 'banana', 'apple'] });
+// Set up the PostgreSQL client with connection details
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'admin',
+  database: 'gym',
 });
+
+// Test the connection to PostgreSQL
+pool.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch((err) => console.error('Error connecting to PostgreSQL:', err));
 
 app.listen(8080, () => {
   console.log('Server started on port 8080');
